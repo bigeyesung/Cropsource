1ce2fe70fabe474258201c98861f2b1caeeda048
contiliosung <sungchenhsi@gmail.com>
2019-11-08 17:14:14 +0000

    -CropHandler.h: bug fixed/ func add, minor change

--- CropHandler.h
+++ CropHandler.h
@@ -10,3 +10,13 @@
 #include <iostream>
 #include <string>
 #include <ctime> //count time
+
+// FFmpeg
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/pixdesc.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+}
