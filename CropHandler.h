9d8709edad209141b4008925fff75572a223be0a
contiliosung <sungchenhsi@gmail.com>
2019-11-08 17:14:02 +0000

    -CropHandler.h: bug fixed/ func add, minor change

---
+++ CropHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "SHMHandler.h"
+#include <fstream>
+//#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h> 
+#include <iostream>
+#include <string>
+#include <ctime> //count time
