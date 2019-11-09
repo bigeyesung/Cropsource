a766d31b802de1c818f0d928a7c0caa27aa55967
contiliosung <sungchenhsi@gmail.com>
2019-11-09 23:55:45 +0000

    -CropHandler.cpp: bug fixed/ func add, minor change

--- CropHandler.cpp
+++ CropHandler.cpp
@@ -1,2 +1,12 @@
 #include "CropHandler.h"
 #include "Mediator.h"
+
+CropHandler::CropHandler(Mediator* mediator)
+{
+	m_cur_frames = 1;
+	p_mediator = mediator;
+	m_need_init = false;
+	m_is_stoped = false;
+	m_crop_thread = NULL;
+	
+}
