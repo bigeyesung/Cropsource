a7319dbbd0cc8373d364bf7e1cba199b88c73923
contiliosung <sungchenhsi@gmail.com>
2019-11-17 18:00:12 +0000

    -SHMHandler.cpp: bug fixed/ func add, minor change

--- SHMHandler.cpp
+++ SHMHandler.cpp
@@ -1,2 +1,10 @@
 #include "SHMHandler.h"
 #include "Mediator.h"
+
+SHMHandler::SHMHandler(Mediator* mediator)
+{
+	p_mediator = mediator;
+	//Is_SHM_Enabled = false;
+	//pkWebcamParamsSHM = NULL;
+	cropparams = NULL;
+}
