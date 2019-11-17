49c5b073f1534d292cc625adee0e2cf96ffdcbea
contiliosung <sungchenhsi@gmail.com>
2019-11-17 18:03:31 +0000

    -WindowsCaptureHandler.h: bug fixed/ func add, minor change

---
+++ WindowsCaptureHandler.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#include "../Modules/DirectXWindowCapture/DirectXWindowCapture.h"
+
+#include "cinder/app/App.h"  //for test
+using namespace ci;
+using namespace ci::app;
+using namespace std;
