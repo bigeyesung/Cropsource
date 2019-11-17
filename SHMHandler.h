3e971bd2388132004bdb9f39e567c8a4c87d4615
contiliosung <sungchenhsi@gmail.com>
2019-11-17 17:57:59 +0000

    -SHMHandler.h: bug fixed/ func add, minor change

---
+++ SHMHandler.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+//#include "WinAPISharedMem_CType.h"
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include "boost\asio.hpp"  //for winsocket conflict
+#include "../../Include/CropParams.h"
+#include "../../Include/WinAPISharedMem_CType.h"
