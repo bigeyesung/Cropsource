5db4c64e8239b0ee09cf015852468619f8b5bc41
contiliosung <sungchenhsi@gmail.com>
2019-11-17 18:08:44 +0000

    -TCPHandler.h: bug fixed/ func add, minor change

---
+++ TCPHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+//#include <boost\asio.hpp>
+
+#include "cinder/gl/gl.h"
+
+#include "../Modules/TCPclient/TCPClientClass.h"
+#include "../Modules/UDPBroadcast/UDPBroadcastClient.h"
+#include "../Modules/DataBreakDown/DataBreakDown.h"
+#include "../Modules/Mesh/MeshModule.h"
+#include "../Modules/VideoModule/VideoLoad.h"
+#include "../Modules/UDPProcess/UdpProcess.h"
+#include "../3rdParty/OpenCloseEXE/OpenCloseEXE.h"
