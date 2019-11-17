9989d61526409d790732ba873b0567940577d687
contiliosung <sungchenhsi@gmail.com>
2019-11-17 17:58:09 +0000

    -SHMHandler.h: bug fixed/ func add, minor change

--- SHMHandler.h
+++ SHMHandler.h
@@ -6,3 +6,12 @@
 #include "boost\asio.hpp"  //for winsocket conflict
 #include "../../Include/CropParams.h"
 #include "../../Include/WinAPISharedMem_CType.h"
+
+class Mediator;
+class SHMHandler
+{
+private:
+	Mediator* p_mediator;
+	bool Is_SHM_Enabled;                                   //decide SHM state 
+	CropParams* cropparams;                                //crop parameter 
+	WinAPISharedMem<CropParams>* pkWebcamParamsSHM;
